212023ff624336ab4de79b796c8803cdd19aa33b
elihudiaz <diazelihu96@gmail.com>
2023-10-11 18:50:14 -0400

    Sending PING command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use async_std::io;
 use async_std::net::TcpStream;
+use async_std::prelude::*;
 
 #[async_std::main]
-async fn main() {
-    let stream = TcpStream::connect("localhost:6379").await;
+async fn main() -> io::Result<()> {
+    let mut stream: TcpStream = TcpStream::connect("localhost:6379").await?;
+    let command: &[u8; 14] = b"*1\r\n$4\r\nPING\r\n";
+    let mut buffer: Vec<u8> = vec![0; 1024];
+
+    stream.write_all(command).await?;
+    let bytes_read: usize = stream.read(&mut buffer).await?;
+
+    println!("{:?}", std::str::from_utf8(&buffer[..bytes_read]));
+    Ok(())
 }
