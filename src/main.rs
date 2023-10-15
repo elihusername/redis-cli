8769789e0851afa995d507b39d3f72d3cec46ef5
elihudiaz <diazelihu96@gmail.com>
2023-10-15 18:30:04 -0400

    Adding proper Custom error handling

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,24 @@ use async_std::net::{TcpStream, ToSocketAddrs};
 use async_std::prelude::*;
 
 #[async_std::main]
-async fn main() -> io::Result<()> {
+async fn main() -> Result<(), RedisError> {
     let mut tcp_client = Client::new("localhost:6379").await?;
-    tcp_client
-        .set("Elihu".into(), "Miami".into())
-        .await
-        .unwrap();
-    println!("{}", tcp_client.get("Elihu".into()).await.unwrap());
+    tcp_client.set("Elihu".into(), "Miami".into()).await?;
+    println!("{}", tcp_client.get("Elihu".into()).await?);
     Ok(())
 }
 
-fn parse_response(buffer: &[u8]) -> Result<&str, Error> {
+fn parse_response(buffer: &[u8]) -> Result<&str, RedisError> {
     if buffer.is_empty() {
-        return Err(Error {});
+        return Err(RedisError::EmptyBuffer);
     }
 
     if buffer[0] == b'-' {
-        return Err(Error {});
+        return Err(RedisError::ResponseError);
     }
+    // TODO: remove utf8 error unwrap
+    // let utf8_result = std::str::from_utf8(&buffer[1..buffer.len() - 2]);
+    // let valid_utf8 = utf8_result.map_err(|_| RedisError::Utf8DecodingError)?;
 
     Ok(std::str::from_utf8(&buffer[1..buffer.len() - 2]).unwrap())
 }
@@ -30,14 +30,14 @@ struct Client {
 }
 
 impl Client {
-    async fn new<A: ToSocketAddrs>(addr: A) -> Result<Client, io::Error> {
+    async fn new<A: ToSocketAddrs>(addr: A) -> Result<Client, RedisError> {
         let stream: TcpStream = TcpStream::connect(addr).await?;
         Ok(Client { stream })
     }
 }
 
 impl Client {
-    async fn get(&mut self, key: String) -> Result<String, Error> {
+    async fn get(&mut self, key: String) -> Result<String, RedisError> {
         let mut buffer: Vec<u8> = vec![];
         let command: RespValues = RespValues::Array(vec![
             RespValues::BulkString(b"GET".to_vec()),
@@ -53,7 +53,7 @@ impl Client {
         Ok(resp.to_owned())
     }
 
-    async fn set(&mut self, key: String, value: String) -> Result<(), Error> {
+    async fn set(&mut self, key: String, value: String) -> Result<(), RedisError> {
         let mut buffer: Vec<u8> = vec![];
         let command: RespValues = RespValues::Array(vec![
             RespValues::BulkString(b"SET".to_vec()),
@@ -70,12 +70,31 @@ impl Client {
         Ok(())
     }
 }
-#[derive(Debug)]
-struct Error {}
 
-impl std::convert::From<io::Error> for Error {
-    fn from(_: io::Error) -> Self {
-        Error {}
+#[derive(Debug)]
+enum RedisError {
+    ConnectionError(io::Error),
+    ResponseError,
+    EmptyBuffer,
+}
+
+impl std::error::Error for RedisError {}
+
+impl std::fmt::Display for RedisError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        match self {
+            RedisError::ConnectionError(io_err) => {
+                write!(f, "Connection error: {}", io_err)
+            }
+            RedisError::ResponseError => write!(f, "Response error"),
+            RedisError::EmptyBuffer => write!(f, "Buffer is empty"),
+        }
+    }
+}
+
+impl From<io::Error> for RedisError {
+    fn from(io_error: io::Error) -> Self {
+        RedisError::ConnectionError(io_error)
     }
 }
 
